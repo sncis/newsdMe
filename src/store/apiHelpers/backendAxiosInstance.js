2bff27799eb6e00d1020801f4ed49225588e8158
snzew <Sitan.zewde@code.berlin>
2021-05-03 17:24:52 +0200

    test

--- src/store/apiHelpers/backendAxiosInstance.js
+++ src/store/apiHelpers/backendAxiosInstance.js
@@ -10,7 +10,7 @@ const backendAxiosInstance = axios.create({
   withCredentials: true,
 });
 
-const getCookies = ()=>
+export const getCookies = ()=>
     document.cookie.split(';').reduce((cookies, item) =>{
       const [name,value] = item.split('=');
       cookies[name] = value;
@@ -19,21 +19,45 @@ const getCookies = ()=>
 
 backendAxiosInstance.interceptors.request.use((config) => {
   const csrfToken = getCookies()['XSRF-TOKEN']
-  console.log("getting cookies")
+  const newCsrfToken = getCookies()["X-CSRF-TOKEN"]
+  console.log("getting all cookies")
   console.log(getCookies())
   console.log("csrf token ")
   console.log(csrfToken)
-  if(csrfToken) config.headers['X-XSRF-TOKEN'] = csrfToken;
 
-  console.log("headers")
+  console.log("new cookie header")
+  console.log(newCsrfToken)
+  console.log("*********")
+  console.log("*******")
+  console.log("*******")
+
+  if(csrfToken) config.headers['X-XSRF-TOKEN'] = csrfToken;
+  if(csrfToken) config.headers['X-CSRF-TOKEN'] = csrfToken;
+
+
+  console.log("headers from request")
   console.log(config.headers)
   console.log("configs")
   console.log(config)
-  console.log("xsrf cheader")
+  console.log("xsrf cheader in request ")
 
   console.log(config.headers['X-XSRF-TOKEN']);
+  console.log(config.headers['X-CSRF-TOKEN']);
+
   return config;
 })
+
+backendAxiosInstance.interceptors.response.use((response) => {
+  const cookies = getCookies()['XSRF-TOKEN']
+  console.log("xsrf cookie")
+  console.log(cookies)
+  console.log("all cookies")
+  console.log(getCookies())
+  console.log("headers")
+  console.log(response.headers)
+  return response;
+})
+
 
 
 export default backendAxiosInstance;

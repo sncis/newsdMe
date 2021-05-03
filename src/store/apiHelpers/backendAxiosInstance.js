68f9bcc0f6bf8b1f5d0122c779d99790cbb828ba
snzew <Sitan.zewde@code.berlin>
2021-05-03 19:33:49 +0200

    test

--- src/store/apiHelpers/backendAxiosInstance.js
+++ src/store/apiHelpers/backendAxiosInstance.js
@@ -8,7 +8,11 @@ const backendAxiosInstance = axios.create({
     'Content-Type': 'application/json;charset=UTF-8',
   },
   withCredentials: true,
+
 });
+
+backendAxiosInstance.xsrfHeaderName= 'X-CSRF-TOKEN';
+backendAxiosInstance.xsrfCookieName= 'CSRF-TOKEN';
 
 export const getCookies = ()=>
     document.cookie.split(';').reduce((cookies, item) =>{
@@ -18,43 +22,60 @@ export const getCookies = ()=>
     },{});
 
 backendAxiosInstance.interceptors.request.use((config) => {
-  const csrfToken = getCookies()['XSRF-TOKEN']
-  const newCsrfToken = getCookies()["X-CSRF-TOKEN"]
-  console.log("getting all cookies")
-  console.log(getCookies())
-  console.log("csrf token ")
-  console.log(csrfToken)
+  const selfToken = Math.random().toString(36).substring(10)
 
-  console.log("new cookie header")
-  console.log(newCsrfToken)
-  console.log("*********")
-  console.log("*******")
-  console.log("*******")
+  //
+  config.xsrfHeaderName= 'X-CSRF-TOKEN';
+  config.xsrfCookieName= 'CSRF-TOKEN';
+  document.cookie = 'CSRF-TOKEN=' + selfToken;
+  config.headers['X-CSRF-TOKEN']= selfToken;
 
-  if(csrfToken) config.headers['X-XSRF-TOKEN'] = csrfToken;
+  console.log("csrf token from request ")
 
-
-  console.log("headers from request")
+  console.log(getCookies()['CSRF-TOKEN'])
+  console.log(config.xsrfCookieName)
+  console.log("in request interceptor")
+  console.log(config.headers.xsrfHeaderName)
   console.log(config.headers)
-  console.log("configs")
-  console.log(config)
-  console.log("xsrf cheader in request ")
 
-  console.log(config.headers['X-XSRF-TOKEN']);
-  console.log(config.headers['X-CSRF-TOKEN']);
+  // const csrfToken = getCookies()['XSRF-TOKEN']
+  // const newCsrfToken = getCookies()["X-CSRF-TOKEN"]
+  // console.log("getting all cookies")
+  // console.log(getCookies())
+  // console.log("csrf token ")
+  // console.log(csrfToken)
+  //
+  // console.log("new cookie header")
+  // console.log(newCsrfToken)
+  // console.log("*********")
+  // console.log("*******")
+  // console.log("*******")
+  //
+  // if(csrfToken) config.headers['X-XSRF-TOKEN'] = csrfToken;
+  //
+  //
+  // console.log("headers from request")
+  // console.log(config.headers)
+  // console.log("configs")
+  // console.log(config)
+  // console.log("xsrf cheader in request ")
+  //
+  // console.log(config.headers['X-XSRF-TOKEN']);
+  // console.log(config.headers['X-CSRF-TOKEN']);
   config.withCredentials = true;
 
   return config;
 })
 
 backendAxiosInstance.interceptors.response.use((response) => {
-  const cookies = getCookies()['XSRF-TOKEN']
-  console.log("xsrf cookie")
+  console.log("response headers**************")
+  const cookies = getCookies()['CSRF-TOKEN']
+  console.log("csrf cookie")
   console.log(cookies)
   console.log("all cookies")
   console.log(getCookies())
-  console.log("headers")
-  console.log(response.headers)
+  console.log("headers name ")
+  console.log(response.headers.xsrfHeaderName);
   return response;
 })
 
